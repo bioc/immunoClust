96e3a5f8f31ed1b376cc08f74ac09d00afb2f347
Till Sörensen <till-antoni.soerensen@charite.de>
2021-03-25 12:35:27 +0100

    testthat problem

--- tests/testthat/test_cell-clust.R
+++ tests/testthat/test_cell-clust.R
@@ -1,18 +1,19 @@
 
-test_that("cell.ME", {
-  
-  data(dat.fcs)
-  data(dat.exp)
-  ## cell.clustering result for dat.fcs
-  r <- dat.exp[[1]]
-  ## apply model parameter to all (unfiltered) events
-  dat.trans <- trans.ApplyToData(r, dat.fcs)
-  r2 <- cell.EM(dat.trans, parameters(r), K=ncls(r),
-              w=weights(r),m=mu(r),s=sigma(r))
-  
+test_that("cell.EM", {
+
+    data(dat.fcs)
+    data(dat.exp)
+    ## cell.clustering result for dat.fcs
+    r <- dat.exp[[1]]
+    ## apply model parameter to all (unfiltered) events
+    dat.trans <- trans.ApplyToData(r, dat.fcs)
+    r2 <- cell.EM(dat.trans, parameters(r), K=ncls(r),
+                w=weights(r),m=mu(r),s=sigma(r))
+
 })
 
-test_that("cell.EM", {
+test_that("cell.ME", {
+
     data(dat.fcs)
     data(dat.exp)
     ## cell.clustering result for dat.fcs
@@ -20,10 +21,11 @@ test_that("cell.EM", {
     ## apply model parameter to all (unfiltered) events
     dat.trans <- trans.ApplyToData(r, dat.fcs)
     r2 <- cell.ME(dat.trans, parameters(r), label=label(r))
-  
+
 })
 
 test_that("cell.FitModel", {
+ 
     data(dat.fcs)
     data(dat.exp)
     r1 <- dat.exp[[1]]
@@ -33,13 +35,17 @@ test_that("cell.FitModel", {
 })
 
 test_that("cell.ClustData", {
+
     data(dat.fcs)
     res <- cell.ClustData(dat.fcs, 5, parameters=c("FSC-A", "SSC-A"))
 
 })
 
 test_that("cell.hclust", {
+
     data(dat.fcs)
     inc <- sample(1:nrow(dat.fcs), 50)
     result <- cell.hclust(exprs(dat.fcs)[inc,])
+
 })
+
