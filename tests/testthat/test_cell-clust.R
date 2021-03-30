9fa82eda92430745fb9b0d630fc7a14f699df4e8
Till Sörensen <till-antoni.soerensen@charite.de>
2021-03-30 09:59:55 +0200

    testthat problems

--- tests/testthat/test_cell-clust.R
+++ tests/testthat/test_cell-clust.R
@@ -10,28 +10,19 @@ test_that("cell.EM", {
     r2 <- cell.EM(dat.trans, parameters(r), K=ncls(r),
                 w=weights(r),m=mu(r),s=sigma(r))
 
-})
-
-test_that("cell.ME", {
-
-    data(dat.fcs)
-    data(dat.exp)
-    ## cell.clustering result for dat.fcs
-    r <- dat.exp[[1]]
-    ## apply model parameter to all (unfiltered) events
-    dat.trans <- trans.ApplyToData(r, dat.fcs)
-    r2 <- cell.ME(dat.trans, parameters(r), label=label(r))
+    expect_is(r2, "immunoClust")
 
 })
 
 test_that("cell.FitModel", {
- 
+
     data(dat.fcs)
     data(dat.exp)
     r1 <- dat.exp[[1]]
     dat.trans <- trans.ApplyToData(r1, dat.fcs)
     r2 <- cell.FitModel(r1, dat.trans)
 
+    expect_is(r2, "immunoClust")
 })
 
 test_that("cell.ClustData", {
@@ -39,6 +30,7 @@ test_that("cell.ClustData", {
     data(dat.fcs)
     res <- cell.ClustData(dat.fcs, 5, parameters=c("FSC-A", "SSC-A"))
 
+    expect_is(res, "immunoClust")
 })
 
 test_that("cell.hclust", {
@@ -46,6 +38,6 @@ test_that("cell.hclust", {
     data(dat.fcs)
     inc <- sample(1:nrow(dat.fcs), 50)
     result <- cell.hclust(exprs(dat.fcs)[inc,])
-
+    expect_len(result, 98)
 })
 
--- tests/testthat/test_trans.R
+++ tests/testthat/test_trans.R
@@ -1,13 +1,14 @@
 
 test_that("transformation", {
   
- data(dat.fcs)
- data(dat.exp)
- dat.trans <- trans.ApplyToData(dat.exp[[1]], dat.fcs)
- 
- r <- cell.Classify(dat.exp[[1]], dat.trans)
- t <- trans.FitToData(r, dat.fcs)
-  
+  data(dat.fcs)
+  data(dat.exp)
+  dat.trans <- trans.ApplyToData(dat.exp[[1]], dat.fcs)
+  expect_is(dat.trans, "flowFrame")
+  r <- cell.Classify(dat.exp[[1]], dat.trans)
+  expect_is(r, "immunoClust")
+  t <- trans.FitToData(r, dat.fcs)
+  expect_len(t,14)
 })
 
 
